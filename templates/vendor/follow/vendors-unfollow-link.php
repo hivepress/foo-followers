595cc232b581f3551c20da3ae8cf6b5d939f36cd
ihor <ihor@hivepress.io>
2022-07-07 21:28:55 +0300

    Add unfollow link and modal

---
+++ templates/vendor/follow/vendors-unfollow-link.php
@@ -0,0 +1,5 @@
+<?php
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+?>
+<a href="#vendors_unfollow_modal" class="button"><?php esc_html_e( 'Unfollow Vendors', 'foo-followers' ); ?></a>
